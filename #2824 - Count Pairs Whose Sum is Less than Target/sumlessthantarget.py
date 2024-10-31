b05da1345cff19106befd81cf24e8ced16208bfc
Kyle Shott <116819916+kyshott@users.noreply.github.com>
2024-10-31 14:45:06 -0400

    Create sumlessthantarget.py

---
+++ #2824 - Count Pairs Whose Sum is Less than Target/sumlessthantarget.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def countPairs(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        count = 0
+
+        for i in range(0, len(nums)):
+            for j in range(i + 1, len(nums)):
+                if(nums[i] + nums[j] < target):
+                    count += 1
+        
+        return count
