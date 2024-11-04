55f993c746aa22099d90af0f265b7a8686ddaf24
Kyle Shott <116819916+kyshott@users.noreply.github.com>
2024-11-03 21:51:25 -0500

    Create alternatemerge.py

---
+++ #1768 - Merge Strings Alternately/alternatemerge.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged = ""
+
+        for i in range(min(len(word1), len(word2))):
+            merged += word1[i]
+            merged += word2[i]
+        
+        if(len(word1) > len(word2)):
+            merged += word1[len(word2):]
+        else:
+            merged += word2[len(word1):]
+        
+        return merged
