d240ccb0290bd674fb721e03d9e37b412ac00b14
Kyle Shott <116819916+kyshott@users.noreply.github.com>
2025-02-13 00:19:46 -0500

    Create thresholdvalue1.cpp

---
+++ #3065 - Minimum Operations to Exceed Threshold Value I/thresholdvalue1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int operations = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] < k) {
+                operations++;
+            }
+        }
+        return operations;
+    }
+};
