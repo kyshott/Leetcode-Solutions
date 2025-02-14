9563b3d5b555711949befae995b3f61c09ee979b
Kyle Shott <116819916+kyshott@users.noreply.github.com>
2025-02-14 02:05:10 -0500

    Create validanagram.cpp

---
+++ #0242 - Valid Anagram/validanagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        if(s.length() != t.length()) {
+            return false;
+        }
+        
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] != t[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
